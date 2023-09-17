3216248cc6061c72cd737d1840b78b12242ca01f
Henrique <henrique.natan32@gmail.com>
2023-09-16 22:06:15 -0300

    input checkbox style e js

--- js/js_post.js
+++ js/js_post.js
@@ -1,5 +1,23 @@
-var checkBox = document.createElement("input");
-    checkBox.type = 'checkbox';
-    if (this.checkBox) {
-        inputItem.style.textDecoration = "line-through";
-    } else {inputItem.style.textDecoration = "none";}
+
+
+// Get all checkboxes and labels
+const checkboxes = document.querySelectorAll('.checkbox');
+const labels = document.querySelectorAll('.form-control');
+
+// Add event listener to each checkbox
+checkboxes.forEach((checkbox, index) => {
+  checkbox.addEventListener('change', function() {
+    // If checkbox is checked, apply text-decoration: line-through to the corresponding label
+    if (checkbox.checked) {
+      labels[index].style.textDecoration = 'line-through';
+    } else {
+      // If checkbox is unchecked, remove text-decoration: line-through from the corresponding label
+      labels[index].style.textDecoration = 'none';
+    }
+  });
+});
+
+
+
+
+
