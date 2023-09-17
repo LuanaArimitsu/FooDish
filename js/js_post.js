4d0ef845f9709e3ac3ad880b1b4d0237eec54f26
Henrique <henrique.natan32@gmail.com>
2023-09-16 21:35:17 -0300

    alteração sidebar

---
+++ js/js_post.js
@@ -0,0 +1,5 @@
+var checkBox = document.createElement("input");
+    checkBox.type = 'checkbox';
+    if (this.checkBox) {
+        inputItem.style.textDecoration = "line-through";
+    } else {inputItem.style.textDecoration = "none";}
